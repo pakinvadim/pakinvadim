6f93b2080198470cb6582295e711df6d91fc2c47
Vadim Pakin <pakinvadim@gmail.com>
2021-10-22 03:02:35 +0300

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,1 +1,15 @@
+using System;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data;
+using System.Globalization;
+using System.Threading;
+using Vesna.Business.Data;
+using Vesna.Forms;
 
+namespace Test {
+	internal static class Program {
+		[STAThread]
+		private static void Main() { }
+	}
+}
