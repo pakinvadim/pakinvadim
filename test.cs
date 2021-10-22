93b01cc5b20e371bb7e9409c0922b2ff53ce49fc
Vadim Pakin <pakinvadim@gmail.com>
2021-10-22 03:02:48 +0300

    Update test.cs

--- test.cs
+++ test.cs
@@ -4,8 +4,6 @@ using System.Data.OleDb;
 using System.Data;
 using System.Globalization;
 using System.Threading;
-using Vesna.Business.Data;
-using Vesna.Forms;
 
 namespace Test {
 	internal static class Program {
